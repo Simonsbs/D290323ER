ba9ef5f42f0f84ebff17eb08ca16373ead8ad06c
Simon B.Stirling <simonsbs@gmail.com>
2023-05-31 18:33:37 +0300

    Added stuff to main js, thanks for the great job!

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,19 @@
+// This is my code
+
+/*
+    sjkdbnfksjdbnhfkjs
+    sdf
+    sdf
+    sdf
+    sdf
+    sdf
+    sdf
+
+    remember the server password is: 1234567890
+
+    The client is dumb!!!
+*/
+
 log("Hello World");
 log("Simon");
 log("Bobo");
