816b612cde7b85372a5b94f21368923a171c7ace
Simon B.Stirling <simonsbs@gmail.com>
2023-06-11 18:13:40 +0300

    added bubble sort

---
+++ JS/bubble.js
@@ -0,0 +1,29 @@
+function bubbleSort(arr){
+    for(let j = 0; j < (arr.length - 1); j++){
+        for (let i = 0; i < (arr.length - 1) - j;i++) {
+            if(arr[i] > arr[i + 1]){
+                swap(arr, i, i + 1);
+            }
+        }
+    }
+
+    return arr;
+}
+
+function swap(arr, fromI, toI){
+    arr.splice(fromI, 2, arr[toI], arr[fromI]);
+
+    /*let cup = arr[fromI];
+    arr[fromI] = arr[toI];
+    arr[toI] = cup;*/
+}
+
+
+let test1 = [64,34,25,12,22,11,90];
+log(test1);
+let result = bubbleSort(test1);
+log(result);
+let test2 = [100,20,-10,-25,0,990];
+log(test2);
+result = bubbleSort(test2);
+log(result);
