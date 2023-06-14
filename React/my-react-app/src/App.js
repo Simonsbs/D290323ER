5d7cc99d78bf9da81ae777e3a8d8d6b5532bb464
Simon B.Stirling <simonsbs@gmail.com>
2023-06-14 21:24:58 +0300

    added exercise

--- React/my-react-app/src/App.js
+++ React/my-react-app/src/App.js
@@ -5,13 +5,19 @@ import { useState } from 'react';
 
 function App() {
   const [emotion, setEmotion] = useState("Happy");
+  const [color, setColor] = useState("Red");
+  
   console.log(setEmotion);
 
   return (
     <div className="App">
+      <img src="asdasd"></img>
       <h1>Simon is: {emotion}</h1>
+      <h1>My color is: {color}</h1>
       <button onClick={() => setEmotion("Angry")}>Angry</button>
       <button onClick={() => setEmotion("Happy")}>Happy</button>
+
+      {/* TODO: Add 2 buttons that change color from red to blue and back */}
     </div>
   );
 }
