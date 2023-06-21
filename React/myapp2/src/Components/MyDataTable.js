d592ca0b6f66b5b9986f8025291d95347f344d13
Simon B.Stirling <simonsbs@gmail.com>
2023-06-21 18:28:45 +0300

    added states

--- React/myapp2/src/Components/MyDataTable.js
+++ React/myapp2/src/Components/MyDataTable.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import MyDataRow from "./MyDataRow";
 
 const listOfPeople = [
@@ -19,8 +20,35 @@ const listOfPeople = [
 }*/
 
 function MyDataTable() {
+    const [people, setPeople] = useState([
+                                            {id: 254, fName: "Simon", lName: "Stirling", age: 18},
+                                            {id: 111, fName: "Alon", lName: "Guy", age: 34},
+                                            {id: 671, fName: "Bob", lName: "Piscapoe", age: 34},
+                                            {id: 52, fName: "Jane", lName: "Doe", age: 52},
+                                        ]);
+    const [counter, setCounter] = useState(0);
+    
+    const addPersonDirect = () => {
+        listOfPeople.push({id: 123, fName: "Emma", lName: "Smith", age: 25});
+        console.log('Added person to list');
+    };
+
+    const addPersonToState = () => {
+        setPeople([
+            ...people,
+            {id: 123, fName: "Emma", lName: "Smith", age: 25}
+        ]);
+    };
+
+    const addToCounter = () => {
+        setCounter(counter + 1);
+    };
+
     return (
-        <>
+        <>  
+            <div>
+                Counter: {counter}
+            </div>
             <h2>My people:</h2>
             <table>
                 <thead>
@@ -52,12 +80,23 @@ function MyDataTable() {
                             //     <MyDataRow {...person} />
                             // ))
 
-                            listOfPeople.map((person) => (
+                            people.map((person) => (
                                 <MyDataRow key={person.id} person={person} />
                             ))
                         }
                 </tbody>
             </table>
+            <button onClick={addPersonDirect}>
+                Add person direct
+            </button>
+
+            <button onClick={addPersonToState}>
+                Add person to state
+            </button>
+            <br />
+            <button onClick={addToCounter}>
+                Add to counter
+            </button>
         </>        
     );
 }
