1cb33632cfa856178716310ed8445b8cbd9c17ae
Simon B.Stirling <simonsbs@gmail.com>
2023-06-25 19:46:03 +0300

    .

--- React/myapp3/src/components/SetFocus.js
+++ React/myapp3/src/components/SetFocus.js
@@ -9,11 +9,12 @@ function SetFocus(){
         <>
             <div>
                 <input type="text" ref={refInput1} placeholder="Im a text box 1" />
-                <input type="text" ref={refInput2} placeholder="Im a text box 2" />
-                <input type="text" ref={refInput3} placeholder="Im a text box 3" />
-
                 <button onClick={() => refInput1.current.focus()}>Focus 1</button>
+                <br/>
+                <input type="text" ref={refInput2} placeholder="Im a text box 2" />
                 <button onClick={() => refInput2.current.focus()}>Focus 2</button>
+                <br/>
+                <input type="text" ref={refInput3} placeholder="Im a text box 3" />
                 <button onClick={() => refInput3.current.focus()}>Focus 3</button>
             </div>
         </>
