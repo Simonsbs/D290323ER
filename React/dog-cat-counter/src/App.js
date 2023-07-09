c2d2755fec8fe746f525db9942eae73334f92103
Simon B.Stirling <simonsbs@gmail.com>
2023-07-09 20:52:05 +0300

    V5

--- React/dog-cat-counter/src/App.js
+++ React/dog-cat-counter/src/App.js
@@ -3,10 +3,13 @@ import CounterComponent from "./components/CounterComponent";
 import CounterComponentV2 from "./components/CounterComponentV2";
 import CounterComponentV3 from "./components/CounterComponentV3";
 import CounterComponentV4 from "./components/CounterComponentV4";
+import CounterComponentV5 from "./components/CounterComponentV5";
 
 function App() {
   return (
     <>
+      <CounterComponentV5 />
+      <hr />
       <CounterComponentV4 />
       <hr />
       <CounterComponentV3 />
---
+++ React/dog-cat-counter/src/components/CounterComponentV5.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+
+const initalState = { cats: 0, dogs: 0 };
+
+function usePetCounter() {
+  const [state, setState] = useState(initalState);
+
+  const addCat = () => {
+    setState({ ...state, cats: state.cats + 1 });
+  };
+
+  const removeCat = () => {
+    setState({ ...state, cats: state.cats - 1 });
+  };
+
+  const addDog = () => {
+    setState({ ...state, dogs: state.dogs + 1 });
+  };
+
+  const removeDog = () => {
+    setState({ ...state, dogs: state.dogs - 1 });
+  };
+
+  const addDogAndCat = () => {
+    setState({ ...state, dogs: state.dogs + 1, cats: state.cats + 1 });
+  };
+
+  const removeDogAndCat = () => {
+    setState({ ...state, dogs: state.dogs - 1, cats: state.cats - 1 });
+  };
+
+  return {
+    state,
+    addCat,
+    removeCat,
+    addDog,
+    removeDog,
+    addDogAndCat,
+    removeDogAndCat,
+  };
+}
+
+function CounterComponentV5() {
+  const {
+    state,
+    addCat,
+    removeCat,
+    addDog,
+    removeDog,
+    addDogAndCat,
+    removeDogAndCat,
+  } = usePetCounter();
+
+  return (
+    <>
+      <h2>useReducer - V5</h2>
+      <hr />
+      <h3>Cats: {state.cats}</h3>
+      <button onClick={addCat}>Add cat</button>
+      <button onClick={removeCat}>Remove cat</button>
+      <hr />
+      <h3>Dogs: {state.dogs}</h3>
+      <button onClick={addDog}>Add dog</button>
+      <button onClick={removeDog}>Remove dog</button>
+      <hr />
+      <button onClick={addDogAndCat}>Add dog and cat</button>
+      <button onClick={removeDogAndCat}>Remove dog and cat</button>
+    </>
+  );
+}
+
+export default CounterComponentV5;
