503a6513d6823c8bc79fd245390387ecb3f2e10f
Simon B.Stirling <simonsbs@gmail.com>
2023-07-16 19:48:10 +0300

    .

--- React/styling/src/components/Bootstrap2.js
+++ React/styling/src/components/Bootstrap2.js
@@ -17,6 +17,32 @@ function Bootstrap2() {
           <div className="col col-sm-2 col-md-3 col-lg-2">Row 3 Col 2</div>
           <div className="col col-sm-2 col-md-6 col-lg-5">Row 3 Col 3</div>
         </div>
+        <div className="row">
+          <div className="col-1">Row 3 Col 1</div>
+          <div className="col-1">Row 3 Col 2</div>
+          <div className="col-1">Row 3 Col 3</div>
+          <div className="col-1">Row 3 Col 4</div>
+          <div className="col-1">Row 3 Col 5</div>
+          <div className="col-1">Row 3 Col 6</div>
+          <div className="col-1">Row 3 Col 7</div>
+          <div className="col-1">Row 3 Col 8</div>
+          <div className="col-1">Row 3 Col 9</div>
+          <div className="col-1">Row 3 Col 10</div>
+          <div className="col-1">Row 3 Col 11</div>
+          <div className="col-1">Row 3 Col 12</div>
+          <div className="col-1">Row 3 Col 13</div>
+          <div className="col-1">Row 3 Col 14</div>
+          <div className="col-1">Row 3 Col 15</div>
+          <div className="col-1">Row 3 Col 16</div>
+        </div>
+        <div className="row">
+          <div className="col-5">Row 4 Col 1</div>
+          <div className="col-9">Row 4 Col 2</div>
+        </div>
+        <div className="row">
+          <div className="col-5 col-md-8">Row 4 Col 1</div>
+          <div className="col-7 col-md-8">Row 4 Col 2</div>
+        </div>
       </div>
     </>
   );
