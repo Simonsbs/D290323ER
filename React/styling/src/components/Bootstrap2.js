36102646d459da83d3f38b43c3efbcd2f50f1960
Simon B.Stirling <simonsbs@gmail.com>
2023-07-16 18:50:29 +0300

    .

--- React/styling/src/components/Bootstrap2.js
+++ React/styling/src/components/Bootstrap2.js
@@ -6,20 +6,16 @@ function Bootstrap2() {
         <div className="row">
           <div className="col">Row 1 Col 1</div>
           <div className="col">Row 1 Col 2</div>
-          <div className="col">Row 1 Col 3</div>
-          <div className="col">Row 1 Col 4</div>
         </div>
         <div className="row">
-          <div className="col">Row 2 Col 1</div>
-          <div className="col">Row 2 Col 2</div>
-          <div className="col">Row 2 Col 3</div>
-          <div className="col">Row 2 Col 4</div>
-          <div className="col">Row 2 Col 5</div>
+          <div className="col-4">Row 2 Col 1</div>
+          <div className="col-4">Row 2 Col 2</div>
+          <div className="col-4">Row 2 Col 3</div>
         </div>
         <div className="row">
-          <div className="col">Row 3 Col 1</div>
-          <div className="col">Row 3 Col 2</div>
-          <div className="col">Row 3 Col 3</div>
+          <div className="col col-sm-8 col-md-3 col-lg-5">Row 3 Col 1</div>
+          <div className="col col-sm-2 col-md-3 col-lg-2">Row 3 Col 2</div>
+          <div className="col col-sm-2 col-md-6 col-lg-5">Row 3 Col 3</div>
         </div>
       </div>
     </>
