629880c6f088e4ba0262b1d593294a0db49c709e
Simon B.Stirling <simonsbs@gmail.com>
2023-07-19 19:50:19 +0300

    progress bar

--- React/styling/src/components/Bootstrap3.js
+++ React/styling/src/components/Bootstrap3.js
@@ -20,6 +20,57 @@ function Bootstrap3() {
               Deadlights coxswain yo-ho-ho draft salmagundi matey gunwalls black
               spot Blimey dance the hempen jig.
             </p>
+
+            <div
+              className="progress mb-3"
+              role="progressbar"
+              aria-label="Animated striped example"
+              aria-valuenow="75"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              <div
+                className="progress-bar progress-bar-striped progress-bar-animated"
+                style={{ width: "75%" }}
+              ></div>
+            </div>
+
+            <div className="progress-stacked mb-3">
+              <div
+                className="progress"
+                role="progressbar"
+                aria-label="Segment one"
+                aria-valuenow="15"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                style={{ width: "15%" }}
+              >
+                <div className="progress-bar"></div>
+              </div>
+              <div
+                className="progress"
+                role="progressbar"
+                aria-label="Segment two"
+                aria-valuenow="30"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                style={{ width: "30%" }}
+              >
+                <div className="progress-bar bg-success"></div>
+              </div>
+              <div
+                className="progress"
+                role="progressbar"
+                aria-label="Segment three"
+                aria-valuenow="20"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                style={{ width: "20%" }}
+              >
+                <div className="progress-bar bg-info"></div>
+              </div>
+            </div>
+
             <h3>Foods my dog likes</h3>
             <ul>
               <li>Canned</li>
