ad650b7c080378c1dbd61f3137cbbd7efc85ec87
Simon B.Stirling <simonsbs@gmail.com>
2023-07-23 18:35:53 +0300

    solution 2 finished

--- React/styling/src/components/ExSolution2.js
+++ React/styling/src/components/ExSolution2.js
@@ -4,14 +4,14 @@ function expect(value) {
       if (value === expectedValue) {
         return true;
       } else {
-        return false;
+        throw new Error("values are not equal");
       }
     },
     notToBe: (expectedValue) => {
       if (value !== expectedValue) {
         return true;
       } else {
-        return false;
+        throw new Error("values are equal");
       }
     },
   };
@@ -25,13 +25,31 @@ function ExSolution2() {
   //console.log(`toBe: ${expect(5).toBe(5)}`);
   //console.log(`notToBe: ${expect(5).notToBe(5)}`);
 
-  console.log(`toBe: ${tmp.toBe(5)}`);
-  console.log(`notToBe: ${tmp.notToBe(5)}`);
+  try {
+    console.log(`toBe 1: ${tmp.toBe(5)}`);
+  } catch (error) {
+    console.log("toBe 1: " + error.message);
+  }
+
+  try {
+    console.log(`notToBe 2: ${tmp.notToBe(5)}`);
+  } catch (error) {
+    console.log("notToBe 2: " + error.message);
+  }
 
   // -----------------------------------------------------
 
-  console.log(`toBe: ${expect(5).toBe(5)}`);
-  console.log(`notToBe: ${expect(6).notToBe(4)}`);
+  try {
+    console.log(`toBe 3: ${expect(5).toBe(4)}`);
+  } catch (error) {
+    console.log("toBe 3: " + error.message);
+  }
+
+  try {
+    console.log(`notToBe 4: ${expect(6).notToBe(4)}`);
+  } catch (error) {
+    console.log("notToBe 4: " + error.message);
+  }
 
   //console.log(tmp);
 
