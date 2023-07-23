e659033a5d2d67d2e94b38304556b82acc2869d1
Simon B.Stirling <simonsbs@gmail.com>
2023-07-23 20:10:30 +0300

    grid form

--- React/styling/src/components/BootstrapForm.js
+++ React/styling/src/components/BootstrapForm.js
@@ -27,6 +27,54 @@ function BootstrapForm() {
         </div>
         <button className="btn btn-primary">Log in</button>
       </form>
+      <hr />
+      <h2>Grid form</h2>
+      <form>
+        <div className="row mb-3">
+          <div className="col">
+            <label htmlFor="inputEmail2" className="form-label">
+              User Email:
+            </label>
+            <input type="email" id="inputEmail2" className="form-control" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <label htmlFor="inputPassword2" className="form-label">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="inputPassword2"
+              className="form-control"
+            />
+          </div>
+        </div>
+        <button className="btn btn-primary mt-3">Log in</button>
+      </form>
+      <hr />
+      <h2>Grid form (Horizontal)</h2>
+      <form>
+        <div className="row mb-3">
+          <div className="col-md-6">
+            <label htmlFor="inputEmail2" className="form-label">
+              User Email:
+            </label>
+            <input type="email" id="inputEmail2" className="form-control" />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="inputPassword2" className="form-label">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="inputPassword2"
+              className="form-control"
+            />
+          </div>
+        </div>
+        <button className="btn btn-primary mt-3">Log in</button>
+      </form>
     </div>
   );
 }
