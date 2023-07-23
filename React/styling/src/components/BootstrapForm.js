fcafedc60a8fd51534dc19e2765395e1d37c34bb
Simon B.Stirling <simonsbs@gmail.com>
2023-07-23 20:49:40 +0300

    extra components

--- React/styling/src/components/BootstrapForm.js
+++ React/styling/src/components/BootstrapForm.js
@@ -7,13 +7,23 @@ function BootstrapForm() {
           <label htmlFor="inputEmail" className="form-label">
             User Email:
           </label>
-          <input type="email" id="inputEmail" className="form-control" />
+          <input
+            type="email"
+            id="inputEmail"
+            className="form-control form-control-lg"
+            disabled
+            value="some@email.com"
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="inputPassword" className="form-label">
             Password:
           </label>
-          <input type="password" id="inputPassword" className="form-control" />
+          <input
+            type="password"
+            id="inputPassword"
+            className="form-control form-control-lg"
+          />
         </div>
         <div className="mb-3 form-check">
           <input
@@ -35,7 +45,13 @@ function BootstrapForm() {
             <label htmlFor="inputEmail2" className="form-label">
               User Email:
             </label>
-            <input type="email" id="inputEmail2" className="form-control" />
+            <input
+              type="email"
+              id="inputEmail2"
+              className="form-control"
+              readOnly
+              value="some@email.com"
+            />
           </div>
         </div>
         <div className="row">
@@ -57,23 +73,76 @@ function BootstrapForm() {
       <form>
         <div className="row mb-3">
           <div className="col-md-6">
-            <label htmlFor="inputEmail2" className="form-label">
+            <label htmlFor="inputEmail3" className="form-label">
               User Email:
             </label>
-            <input type="email" id="inputEmail2" className="form-control" />
+            <input type="email" id="inputEmail3" className="form-control" />
           </div>
           <div className="col-md-6">
-            <label htmlFor="inputPassword2" className="form-label">
+            <label htmlFor="inputPassword3" className="form-label">
               Password:
             </label>
             <input
               type="password"
-              id="inputPassword2"
+              id="inputPassword3"
               className="form-control"
             />
           </div>
         </div>
         <button className="btn btn-primary mt-3">Log in</button>
+      </form>
+      <h2>Grid form with floating labels</h2>
+      <form>
+        <div className="row mb-3">
+          <div className="col-md-6 form-floating">
+            <input
+              type="email"
+              id="inputEmail4"
+              className="form-control"
+              placeholder="something@something.com"
+            />
+            <label htmlFor="inputEmail4" className="form-label">
+              User Email:
+            </label>
+          </div>
+          <div className="col-md-6 form-floating">
+            <input
+              type="password"
+              id="inputPassword4"
+              className="form-control"
+            />
+            <label htmlFor="inputPassword4" className="form-label">
+              Password:
+            </label>
+          </div>
+        </div>
+        <button className="btn btn-primary mt-3">Log in</button>
+      </form>
+      <h2>Other stuff</h2>
+      <form>
+        <div className="mb-5">
+          <label htmlFor="uploadFile" className="form-label">
+            Upload a file:
+          </label>
+          <input type="file" id="uploadFile" className="form-control" />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="selectSize" className="form-label">
+            Select a size:
+          </label>
+          <select id="selectSize" className="form-select">
+            <option selected>Select a size</option>
+            <option>Big</option>
+            <option>Small</option>
+            <option>Medium</option>
+          </select>
+        </div>
+        <div className="mb-5">
+          <label htmlFor="priceRange" className="form-label">
+            Select Range:
+          </label>
+          <input type="range" id="priceRange" className="form-range" />
+        </div>
       </form>
     </div>
   );
