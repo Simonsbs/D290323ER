8a2c50de520b72c6f1594ac07afcd1c1f1cdb4d0
Simon B.Stirling <simonsbs@gmail.com>
2023-08-02 19:55:54 +0300

    added state and function

--- React/user-manager/src/App.js
+++ React/user-manager/src/App.js
@@ -2,16 +2,6 @@ import "./App.css";
 import MyRouter from "./components/MyRouter";
 
 function App() {
-  const users = [
-    {
-      id: 123,
-      firstName: "Simon",
-      lastName: "Stirling",
-      email: "a@b.com",
-      password: "1234",
-    },
-  ];
-
   return <MyRouter />;
 }
 
