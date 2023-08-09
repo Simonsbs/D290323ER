5737e811c64efbe9c143f321b3cf2358bebbc4fe
Simon B.Stirling <simonsbs@gmail.com>
2023-08-09 21:20:28 +0300

    .

--- React/learning-objects/src/App.js
+++ React/learning-objects/src/App.js
@@ -7,15 +7,24 @@ function App() {
     name: "Simon",
     age: 20,
     color: "Green",
+    sayHello: () => {
+      console.log(`Hey`);
+    },
   };
 
+  person.sayHello();
   //person.color = "Pink";
 
   person = {
     name: "Bob",
     age: 30,
     color: "Purple",
+    sayHello: () => {
+      console.log(`Hey`);
+    },
   };
+
+  person.sayHello();
 
   //person.color = "Yellow";
 
@@ -25,12 +34,40 @@ function App() {
   //   color: "Blue",
   // };
 
+  // Constructor Functions
+
+  function Dog(name, age, color) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+
+    this.bark = () => {
+      console.log(`Woof, my name is ${name}`);
+    };
+  }
+
+  console.log(person);
+  console.log(Dog);
+
+  let simonsDog = new Dog("Fido", 12, "Black");
+  simonsDog = new Dog("Rex", 12, "Black");
+
+  console.log(simonsDog);
+
+  simonsDog.bark();
+
   return (
     <>
       <ul>
         <li>Name: {person.name}</li>
         <li>Age: {person.age}</li>
         <li>Color: {person.color}</li>
+      </ul>
+      <hr />
+      <ul>
+        <li>Name: {simonsDog.name}</li>
+        <li>Age: {simonsDog.age}</li>
+        <li>Color: {simonsDog.color}</li>
       </ul>
     </>
   );
