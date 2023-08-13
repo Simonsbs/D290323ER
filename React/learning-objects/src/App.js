276515338050999ce788cb72c0a15facb9282d61
Simon B.Stirling <simonsbs@gmail.com>
2023-08-13 18:47:29 +0300

    classes

--- React/learning-objects/src/App.js
+++ React/learning-objects/src/App.js
@@ -56,9 +56,61 @@ function App() {
   let simonsDog = new Dog("Fido", 12, "Black");
   simonsDog = new Dog("Rex", 12, "Black");
 
-  console.log(simonsDog);
+  //console.log(simonsDog);
 
   //simonsDog.bark();
+
+  // Object.create()
+
+  const personProto = {
+    name: "",
+    sayHello: () => {
+      console.log(`Hey`);
+    },
+  };
+
+  const p1 = Object.create(personProto);
+  //p1.name = "Simon";
+  const p2 = Object.create(personProto);
+  //p2.name = "Bob";
+
+  // console.log(p1);
+  // console.log(p2);
+
+  // p1.sayHello();
+  // p2.sayHello();
+
+  // Classes
+  class Person {
+    constructor(firstName, lastName, age) {
+      this.fullName = firstName + " " + lastName;
+      this.age = age;
+
+      this.planetOfLiving = "Earth";
+    }
+
+    sayHello() {
+      console.log(`${this.fullName} says Hey from class obj`);
+    }
+  }
+
+  /* 
+    Book class:
+    1. title
+    2. author
+    3. year of publish
+    4. print function: print full details (prints all props to the console)
+
+    Create 3 object instances with unique values and call the print function on each one
+
+  */
+  console.log(Person);
+
+  const c1 = new Person("Simon", "Stirling", 20);
+  const c2 = new Person("Bob", "Smith", 30);
+
+  c1.sayHello();
+  c2.sayHello();
 
   return (
     <>
