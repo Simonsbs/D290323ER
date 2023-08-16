bbf94994bdfd49c26fdea2944d5e92716798ef54
Simon B.Stirling <simonsbs@gmail.com>
2023-08-16 18:23:18 +0300

    human updates

--- React/learning-objects/src/Human.js
+++ React/learning-objects/src/Human.js
@@ -1,10 +1,19 @@
 class Human {
-  constructor(name) {
+  constructor(name, age) {
     this.name = name;
+    this.age = age;
   }
 
   describe() {
     return `Im a Human and my name is ${this.name}`;
+  }
+
+  changeMyNameToBob() {
+    this.name = "Bob";
+  }
+
+  itMyBirthDay() {
+    this.age += 1;
   }
 }
 
