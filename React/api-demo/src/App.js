ce5113c4d8d89fc2b06534ead35d5b56d2f08732
Simon B.Stirling <simonsbs@gmail.com>
2023-08-20 21:17:00 +0300

    added fetch

--- React/api-demo/src/App.js
+++ React/api-demo/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/simonsbs")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setUser(data);
+      })
+      .catch((error) => console.error("api error", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>My API Demo</h1>
+      {user ? (
+        <ul>
+          <li>login: {user.login}</li>
+          <li>id: {user.id}</li>
+        </ul>
+      ) : (
+        <>No data</>
+      )}
+    </>
   );
 }
 
