9a9d56ebda0f1fb4659c96c7cf2eccf50b8cdbd0
Simon B.Stirling <simonsbs@gmail.com>
2024-01-31 18:26:46 +0200

    .

---
+++ React/cards2/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">Simon Is Cool</div>;
+}
+
+export default App;
---
+++ Review/Lesson1-1/Casting/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting {
+	internal class Program {
+		static void Main(string[] args) {
+			int num = 5;
+
+			// Implicit casting
+			double d = num;
+
+			double d2 = 5.0;
+
+			// Explicit casting
+			int num2 = (int)d2;
+
+			Console.WriteLine("num2 = " + num2);
+
+
+			long l = int.MaxValue + 10L;
+			int i = (int)l;
+
+			Console.WriteLine("i=" + i);
+
+			int i2 = 1; 
+			long l2 = i2;
+
+			
+
+			Console.ReadKey();
+		}
+	}
+}
