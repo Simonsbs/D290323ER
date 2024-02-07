0ed197e929932cbb452d15679bb857075da203b8
Simon B.Stirling <simonsbs@gmail.com>
2024-02-07 19:52:24 +0200

    .

--- OOP/Lesson2/Calculator.cs
+++ OOP/Lesson2/Calculator.cs
@@ -7,11 +7,29 @@ using System.Threading.Tasks;
 namespace Lesson2 {
 	internal class Calculator {
 		public int Add(int a, int b) {
-			return a + b;
+			Console.WriteLine("before");
+			int[] vals = new int[] { a, b };
+			int result = Add(vals);
+			Console.WriteLine("after");
+			
+			return result;
+
+			// return a + b;
 		}
 
 		public int Add(int a, int b, int c) {
-			return a + b + c;
+			// example to call the 2 param Add method
+			return Add(Add(a, b), c);
+
+			// example to call the 3 param Add method (recursive)
+			// return Add(a, b, c);
+
+			// example to call the params Add method
+			Console.WriteLine("before");
+			int[] vals = new int[] { a, b, c };
+			int result = Add(vals);
+			Console.WriteLine("after");
+			return result;
 		}
 
 		public int Add(params int[] values) {
