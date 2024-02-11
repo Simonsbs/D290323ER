84910d22efca4414aa72a168d398a1ffe2d3d19c
Simon B.Stirling <simonsbs@gmail.com>
2024-02-11 18:07:20 +0200

    Lesson3

---
+++ OOP/Lesson3/ConvertionTools.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3 {
+	internal static class ConvertionTools {
+		public static double ConvertFahrenheitToCelcius(double fahrenheit) {
+			return (fahrenheit - 32.0) * 5.0 / 9.0;
+		}
+
+		public static double ConvertCelciusToFahrenheit(double celcius) {
+			return (celcius * 9.0 / 5.0) + 32.0;
+		}
+
+		public static double ConvertMilesToKilometers(double miles) {
+			return miles * 1.609344;
+		}
+
+		public static double ConvertKilometersToMiles(double kilometers) {
+			return kilometers / 1.609344;
+		}
+
+		public static double ConvertPoundsToKilograms(double pounds) {
+			return pounds * 0.45359237;
+		}
+
+		public static double ConvertKilogramsToPounds(double pounds) {
+			return pounds / 0.45359237;
+		}
+	}
+}
---
+++ OOP/Lesson3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lesson3 {
+	internal class Program {
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"50F = {ConvertionTools.ConvertFahrenheitToCelcius(50.0)}C");
+			Console.WriteLine($"10F = {ConvertionTools.ConvertCelciusToFahrenheit(10.0)}C");
+			Console.WriteLine("----------------------------");
+			Console.WriteLine($"1 Mile = {ConvertionTools.ConvertMilesToKilometers(1.0)}");
+			Console.WriteLine($"1 Kilometer = {ConvertionTools.ConvertKilometersToMiles(1.0)}");
+			Console.WriteLine("----------------------------");
+			Console.WriteLine($"1 pound = {ConvertionTools.ConvertPoundsToKilograms(1.0)}");
+			Console.WriteLine($"1 kilo = {ConvertionTools.ConvertKilogramsToPounds(1.0)}");
+		}
+	}
+}
