5c90c4db946ddaaf749d228f81f23beb5e69e6c6
Simon B.Stirling <simonsbs@gmail.com>
2024-02-11 18:47:05 +0200

    ex readonly

--- OOP/Lesson3/Program.cs
+++ OOP/Lesson3/Program.cs
@@ -39,6 +39,36 @@ namespace Lesson3 {
 			Data d2 = d1;
 			d1.value = 456;
 			Console.WriteLine(d2.value);*/
+
+
+			/*
+			 *
+			   Create a Class with Readonly Fields
+			   
+			   Define a class named UserProfile.
+			   Inside UserProfile, declare two readonly fields: userId (of type int) and creationDate (of type DateTime).
+			   Initialize Readonly Fields
+			   
+			   Create a constructor for the UserProfile class that assigns values to both readonly fields.
+			   userId should be assigned a new int (can use random if feeling lucky).
+			   creationDate should be assigned the current date and time.
+			   ** Attempt ** to Modify Readonly Fields
+			   
+			   Add a method named UpdateProfile to the UserProfile class.
+			   Inside UpdateProfile, ** attempt ** to modify the values of userId and creationDate.
+			   Observe and note the compiler errors.
+			   Read and Access Readonly Fields
+			   
+			   Create a method in the UserProfile class named DisplayProfileInfo that prints the values of userId and 
+			   creationDate to the console.
+			   Test Your Class
+			   
+			   In the Main method of your application, create an instance of UserProfile.
+			   Call the DisplayProfileInfo method to display the initial values of userId and creationDate.
+			   ** Attempt ** to call UpdateProfile and observe the result.
+			 *
+			 *
+			 */
 		}
 	}
 }
