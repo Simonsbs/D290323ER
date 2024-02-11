a32f3c5d3f64fa2c25616a942ee58afa57acecaf
Simon B.Stirling <simonsbs@gmail.com>
2024-02-11 18:43:45 +0200

    Readonly

---
+++ OOP/Lesson3/Example1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3 {
+	internal class Example1 {
+		public readonly int myID;
+		public readonly Data data;
+
+		public Example1() {
+			myID = 456;
+			data = new Data();
+		}
+
+		public void DoStuff() {
+			// Cant do this since the data field is readonly
+			//data = new Data();
+
+			data.value = 789;
+		}
+	}
+
+	public class Data {
+		public int value = 0;
+	}
+}
--- OOP/Lesson3/Program.cs
+++ OOP/Lesson3/Program.cs
@@ -1,8 +1,9 @@
-﻿namespace Lesson3 {
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lesson3 {
 	internal class Program {
-		static void Main(string[] args)
-		{
-			Console.WriteLine($"50F = {ConvertionTools.ConvertFahrenheitToCelcius(50.0)}C");
+		static void Main(string[] args) {
+			/*Console.WriteLine($"50F = {ConvertionTools.ConvertFahrenheitToCelcius(50.0)}C");
 			Console.WriteLine($"10F = {ConvertionTools.ConvertCelciusToFahrenheit(10.0)}C");
 			Console.WriteLine("----------------------------");
 			Console.WriteLine($"1 Mile = {ConvertionTools.ConvertMilesToKilometers(1.0)}");
@@ -10,6 +11,34 @@
 			Console.WriteLine("----------------------------");
 			Console.WriteLine($"1 pound = {ConvertionTools.ConvertPoundsToKilograms(1.0)}");
 			Console.WriteLine($"1 kilo = {ConvertionTools.ConvertKilogramsToPounds(1.0)}");
+			*/
+			
+			Example1 e1 = new Example1();
+			//e1.myID = 123;
+			
+			// Cant do this since the data field is readonly
+			//e1.data = new Data();
+			e1.data.value = 789;
+			
+			Console.WriteLine(e1.myID);
+
+
+			/*
+			int a = 123;
+			Console.WriteLine(a);
+			int b = a;
+			a = 456;
+			Console.WriteLine(b);
+
+			*/
+			/*
+			Data d1 = new Data();
+			d1.value = 123;
+			Console.WriteLine(d1.value);
+
+			Data d2 = d1;
+			d1.value = 456;
+			Console.WriteLine(d2.value);*/
 		}
 	}
 }
