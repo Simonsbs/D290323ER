fcc5d21ac00b0ff4e3bd33d9a85216806a97ac7c
Simon B.Stirling <simonsbs@gmail.com>
2024-02-18 18:35:16 +0200

    Lesson4

---
+++ OOP/Lesson4/Cat.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4 {
+	internal class Cat {
+		private string name;
+		private int age;
+		private string color;
+		private int livesLeft;
+
+		public void PrintIsAlive() {
+			if (livesLeft != 0) {
+				Console.WriteLine("Im alive");
+			}
+		}
+
+		public int GetFavoriteNumber() {
+			Random rnd = new Random();
+			return rnd.Next(10, 50);
+		}
+
+		public int FavoriteNumber {
+			get {
+				Random rnd = new Random();
+				return rnd.Next(10, 50);
+			}
+			set {
+				Console.WriteLine("Sorry, nothing to set");
+			}
+		}
+
+		public string FullName {
+			get {
+				return name + $" | {age} | {color}";
+			}
+			set {
+				Console.WriteLine("Sorry, nothing to set");
+			}
+		}
+
+		public string Name {
+			get {
+				return name;
+			}
+			set {
+				name = value;
+			}
+		}
+
+		public int Age {
+			get {
+				return age;
+			}
+			set {
+				age = value;
+			}
+		}
+
+		public string Color {
+			get {
+				return color;
+			}
+			set {
+				color = value;
+			}
+		}
+
+		public int LivesLeft {
+			get {
+				if (name == "Simon") {
+					return int.MaxValue;
+				}
+				return livesLeft;
+			}
+			set {
+				if (value < 0) {
+					Console.WriteLine("Age must be greater than 0");
+					return;
+				}
+				livesLeft = value;
+			}
+		}
+	}
+}
---
+++ OOP/Lesson4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Lesson4 {
+	internal class Program {
+		static void Main(string[] args) {
+			Cat c1 = new Cat();
+
+			c1.Name = "Meowser";
+			c1.LivesLeft = 5;
+			Console.WriteLine($"{c1.Name} has {c1.LivesLeft} lives left");
+			c1.LivesLeft = -8;
+			Console.WriteLine($"{c1.Name} has {c1.LivesLeft} lives left");
+			c1.Color = "Ginger";
+			Console.WriteLine($"{c1.Name}'s favorite number is {c1.FavoriteNumber}");
+
+			Cat c2 = new Cat();
+			c2.Name = "Simon";
+			c2.LivesLeft = 2;
+			c2.Color = "Brown";
+			Console.WriteLine($"{c2.Name} has {c2.LivesLeft} lives left");
+			Console.WriteLine($"{c2.Name}'s favorite number is {c2.FavoriteNumber}");
+			Console.WriteLine($"{c2.Name}'s favorite number is {c2.GetFavoriteNumber()}");
+			Console.WriteLine($"{c2.Name}'s full name is: {c2.FullName}");
+
+
+			// Task: Create a 'Product' class inside a new file named 'Product.cs'.
+			// The 'Product' class should have the following properties:
+			// 1. Name - Read/Write property of type string.
+			// 2. Price - Read/Write property of type decimal.
+			// 3. ProductCode - Read-only property of type string. This should be auto-generated in the constructor.
+
+		}
+	}
+}
