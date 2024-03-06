bb8e2e2ebb5bb0aa8549388eef056a7876781371
Simon B.Stirling <simonsbs@gmail.com>
2024-03-06 19:51:16 +0200

    The most significant changes include the addition of a new method `AddItem()` in the `Program.cs` file, modifications to the `Checkin()` and `Checkout()` methods in the same file, and the addition of two new configuration files `LibrarySystem.deps.json` and `LibrarySystem.runtimeconfig.json`.
    1. A new method `AddItem()` was added to the `Program.cs` file. This method allows users to add new items such as books, magazines, or DVDs to the library. The user is prompted to enter the item's id, title, and year published, along with additional information depending on the type of item. The new item is then added to the `libraryItems` list.
    
    2. The `Checkin()` and `Checkout()` methods in the `Program.cs` file were modified. The LINQ code used to find the item to check in or out was replaced with a foreach loop that iterates over the `libraryItems` list. If the item is found and it implements the `ICheckable` interface, the `CheckIn()` or `CheckOut()` method is called on the item.
    
    3. A new `LibrarySystem.deps.json` file was added. This file describes the dependencies of the LibrarySystem application, specifying that the application targets .NET Core 8.0 and includes a reference to the `LibrarySystem.dll` file.
    
    4. A new `LibrarySystem.runtimeconfig.json` file was added. This file configures runtime settings for the .NET application, including the target framework, framework name and version, and a configuration property to disable the use of BinaryFormatter for serialization due to potential security risks.

--- OOP/LibrarySystem/Program.cs
+++ OOP/LibrarySystem/Program.cs
@@ -48,13 +48,70 @@ namespace LibrarySystem {
 						Checkin();
 						break;
 					case "4":
-
+						AddItem();
 						break;
 					case "E":
 						isRunning = false;
 						break;
 				}
 			}
+		}
+
+		private static void AddItem() {
+			Console.WriteLine("What type of item do you want to add:");
+			Console.WriteLine("1. Book");
+			Console.WriteLine("2. Magazine");
+			Console.WriteLine("3. DVD");
+			Console.Write("Enter your choice: ");
+			string choice = Console.ReadLine();
+
+			Console.WriteLine("enter the item id:");
+			int id = int.Parse(Console.ReadLine());
+			Console.WriteLine("enter the item title:");
+			string title = Console.ReadLine();
+			Console.WriteLine("enter the item year published:");
+			int yearPublished = int.Parse(Console.ReadLine());
+
+			switch (choice) {
+				case "1":
+					Book book = new Book {
+						ID = id,
+						Title = title,
+						YearPublished = yearPublished
+					};
+					Console.WriteLine("enter the item author:");
+					book.Author = Console.ReadLine();
+					Console.WriteLine("enter the item genre:");
+					book.Genre = Console.ReadLine();
+					libraryItems.Add(book);
+					break;
+				case "2":
+					Magazine magazine = new Magazine {
+						ID = id,
+						Title = title,
+						YearPublished = yearPublished
+					};
+					Console.WriteLine("enter the item issue number:");
+					magazine.IssueNumber = int.Parse(Console.ReadLine());
+					Console.WriteLine("enter the item category:");
+					magazine.Category = Console.ReadLine();
+					libraryItems.Add(magazine);
+					break;
+				case "3":
+					DVD dvd = new DVD {
+						ID = id,
+						Title = title,
+						YearPublished = yearPublished
+					};
+					Console.WriteLine("enter the item director:");
+					dvd.Director = Console.ReadLine();
+					Console.WriteLine("enter the item duration:");
+					dvd.Duration = int.Parse(Console.ReadLine());
+					libraryItems.Add(dvd);
+					break;
+			}
+
+			Console.WriteLine("New item added");
 		}
 
 		private static void DisplayAll() {
@@ -66,9 +123,9 @@ namespace LibrarySystem {
 		private static void Checkin() {
 			Console.Write("Enter the ID of the item you want to check in: ");
 			int id = int.Parse(Console.ReadLine());
-			
+
 			//LibraryItem itemToCheckIn = libraryItems.FirstOrDefault(item => item.ID == id);
-			
+
 			LibraryItem itemToCheckIn = null;
 			foreach (LibraryItem item in libraryItems) {
 				if (item.ID == id) {
@@ -76,7 +133,7 @@ namespace LibrarySystem {
 					break;
 				}
 			}
-			
+
 			if (itemToCheckIn != null) {
 				ICheckable checkable = itemToCheckIn as ICheckable;
 				if (checkable != null) {
@@ -92,9 +149,9 @@ namespace LibrarySystem {
 		private static void Checkout() {
 			Console.Write("Enter the ID of the item you want to check out: ");
 			int id = int.Parse(Console.ReadLine());
-			
+
 			//LibraryItem itemToCheckIn = libraryItems.FirstOrDefault(item => item.ID == id);
-			
+
 			LibraryItem toCheckOut = null;
 			foreach (LibraryItem item in libraryItems) {
 				if (item.ID == id) {
@@ -102,7 +159,7 @@ namespace LibrarySystem {
 					break;
 				}
 			}
-			
+
 			if (toCheckOut != null) {
 				ICheckable checkable = toCheckOut as ICheckable;
 				if (checkable != null) {
